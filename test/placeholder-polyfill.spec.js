4b726ba4c44de4f73eb6eea7c2d1984ef4e921b9
Vladimir Mollov <vmollov@gmail.com>
2015-12-24 06:58:26 -0500

    Refactoring test

--- test/placeholder-polyfill.spec.js
+++ test/placeholder-polyfill.spec.js
@@ -2,32 +2,31 @@ describe('vm.utils', function () {
     'use strict';
 
     describe('DIR: placeholder', function () {
-        var scope, element, $compile, $q, $timeout,
-            placeholderClass = 'vm-utils-placeholder-polyfill',
-
-            compileDirective = function () {
-                element = $compile(element)(scope);
-                scope.$digest();
-                $timeout.flush();
-            };
 
         beforeEach(function () {
             module('vm.utils');
 
             inject(function ($injector) {
-                scope = $injector.get('$rootScope').$new();
-                $compile = $injector.get('$compile');
-                $q = $injector.get('$q');
-                $timeout = $injector.get('$timeout');
+                this.scope = $injector.get('$rootScope').$new();
+                this.compile = $injector.get('$compile');
+                this.q = $injector.get('$q');
+                this.timeout = $injector.get('$timeout');
 
-                scope.testModel = '';
-                element = '<input ng-model="testModel" placeholder="test placeholder" />';
+                this.scope.testModel = '';
+                this.element = '<input ng-model="testModel" placeholder="test placeholder" />';
+
+                this.compileDirective = function () {
+                    this.element = this.compile(this.element)(this.scope);
+                    this.scope.$digest();
+                    this.timeout.flush();
+                };
+                this.placeholderClass = 'vm-utils-placeholder-polyfill';
             });
         });
 
         it('should use the scope of the invoking element', function () {
-            compileDirective();
-            expect(element.scope()).toBe(scope);
+            this.compileDirective();
+            expect(this.element.scope()).toBe(this.scope);
         });
 
         describe('CASE: placeholder is provided by HTML and polyfill is not needed', function () {
@@ -45,8 +44,8 @@ describe('vm.utils', function () {
             });
 
             it('should exit without doing anything', function () {
-                compileDirective();
-                expect(element.val()).toBe('');
+                this.compileDirective();
+                expect(this.element.val()).toBe('');
             });
         });
 
@@ -67,123 +66,123 @@ describe('vm.utils', function () {
             });
 
             it('should use the value of the element as a placeholder without interfering with ngModel', function () {
-                compileDirective();
-                expect(element.val()).toBe('test placeholder');
-                expect(scope.testModel).toBe('');
-                expect(element.hasClass(placeholderClass)).toBe(true);
+                this.compileDirective();
+                expect(this.element.val()).toBe('test placeholder');
+                expect(this.scope.testModel).toBe('');
+                expect(this.element.hasClass(this.placeholderClass)).toBe(true);
             });
             it('should not be used if the input element has a value', function () {
-                element = '<input placeholder="test placeholder" value="Test Value" />';
-                compileDirective();
-                expect(element.val()).toBe('Test Value');
-                expect(element.hasClass(placeholderClass)).toBe(false);
+                this.element = '<input placeholder="test placeholder" value="Test Value" />';
+                this.compileDirective();
+                expect(this.element.val()).toBe('Test Value');
+                expect(this.element.hasClass(this.placeholderClass)).toBe(false);
             });
             it('should not start out displayed if the input element is bound to ng-model with a truthy value', function () {
-                scope.testModel = 'TestInitialVal';
-                compileDirective();
-                expect(element.val()).toBe('TestInitialVal');
-                expect(element.hasClass(placeholderClass)).toBe(false);
+                this.scope.testModel = 'TestInitialVal';
+                this.compileDirective();
+                expect(this.element.val()).toBe('TestInitialVal');
+                expect(this.element.hasClass(this.placeholderClass)).toBe(false);
             });
             it('should update the placeholder text if the placeholder attribute value changes', function () {
-                scope.testPlaceholder = 'Test 1';
-                element = '<input ng-model="testModel" ng-attr-placeholder="{{testPlaceholder}}" />';
-                compileDirective();
-                expect(element.hasClass(placeholderClass)).toBe(true);
-                expect(element.val()).toBe('Test 1');
-                scope.testPlaceholder = 'Test 2';
-                scope.$digest();
-                $timeout.flush();
-                expect(element.val()).toBe('Test 2');
+                this.scope.testPlaceholder = 'Test 1';
+                this.element = '<input ng-model="testModel" ng-attr-placeholder="{{testPlaceholder}}" />';
+                this.compileDirective();
+                expect(this.element.hasClass(this.placeholderClass)).toBe(true);
+                expect(this.element.val()).toBe('Test 1');
+                this.scope.testPlaceholder = 'Test 2';
+                this.scope.$digest();
+                this.timeout.flush();
+                expect(this.element.val()).toBe('Test 2');
             });
             it('should show/hide the placeholder correctly depending on the element value', function () {
-                compileDirective();
-                expect(element.val()).toBe('test placeholder');
-                expect(scope.testModel).toBe('');
-                expect(element.hasClass(placeholderClass)).toBe(true);
+                this.compileDirective();
+                expect(this.element.val()).toBe('test placeholder');
+                expect(this.scope.testModel).toBe('');
+                expect(this.element.hasClass(this.placeholderClass)).toBe(true);
 
-                element.triggerHandler('focus');
-                expect(element.val()).toBe('');
-                expect(element.hasClass(placeholderClass)).toBe(false);
-                element.val('Changed');
-                element.triggerHandler('blur');
-                expect(element.hasClass(placeholderClass)).toBe(false);
+                this.element.triggerHandler('focus');
+                expect(this.element.val()).toBe('');
+                expect(this.element.hasClass(this.placeholderClass)).toBe(false);
+                this.element.val('Changed');
+                this.element.triggerHandler('blur');
+                expect(this.element.hasClass(this.placeholderClass)).toBe(false);
 
-                element.triggerHandler('focus');
-                expect(element.hasClass(placeholderClass)).toBe(false);
-                element.val('');
-                element.triggerHandler('blur');
-                $timeout.flush();
-                expect(element.hasClass(placeholderClass)).toBe(true);
-                expect(element.val()).toBe('test placeholder');
+                this.element.triggerHandler('focus');
+                expect(this.element.hasClass(this.placeholderClass)).toBe(false);
+                this.element.val('');
+                this.element.triggerHandler('blur');
+                this.timeout.flush();
+                expect(this.element.hasClass(this.placeholderClass)).toBe(true);
+                expect(this.element.val()).toBe('test placeholder');
             });
 
             describe('CASE: input element is a child of form element', function () {
                 var elementForm;
 
                 beforeEach(function () {
-                    element = '<form><input ng-model="testModel" placeholder="test placeholder" /></form>';
-                    compileDirective();
-                    elementForm = element;
-                    element = elementForm.find('input');
+                    this.element = '<form><input ng-model="testModel" placeholder="test placeholder" /></form>';
+                    this.compileDirective();
+                    this.elementForm = this.element;
+                    this.element = this.elementForm.find('input');
                 });
 
                 it('should hide placeholder on form.submit', function () {
-                    expect(element.val()).toBe('test placeholder');
-                    expect(element.hasClass(placeholderClass)).toBe(true);
+                    expect(this.element.val()).toBe('test placeholder');
+                    expect(this.element.hasClass(this.placeholderClass)).toBe(true);
 
-                    elementForm.triggerHandler('submit');
-                    expect(element.val()).toBe('');
-                    expect(element.hasClass(placeholderClass)).toBe(false);
+                    this.elementForm.triggerHandler('submit');
+                    expect(this.element.val()).toBe('');
+                    expect(this.element.hasClass(this.placeholderClass)).toBe(false);
                 });
                 it('should show placeholder on form.reset', function () {
-                    element.triggerHandler('focus');
-                    element.val('Changed');
-                    element.triggerHandler('blur');
-                    expect(element.hasClass(placeholderClass)).toBe(false);
+                    this.element.triggerHandler('focus');
+                    this.element.val('Changed');
+                    this.element.triggerHandler('blur');
+                    expect(this.element.hasClass(this.placeholderClass)).toBe(false);
 
-                    elementForm.triggerHandler('reset');
-                    $timeout.flush();
-                    expect(element.val()).toBe('test placeholder');
-                    expect(element.hasClass(placeholderClass)).toBe(true);
+                    this.elementForm.triggerHandler('reset');
+                    this.timeout.flush();
+                    expect(this.element.val()).toBe('test placeholder');
+                    expect(this.element.hasClass(this.placeholderClass)).toBe(true);
                 });
             });
 
             describe('CASE: input element is of type password', function () {
                 beforeEach(function () {
-                    scope.testInterpolated = 'testVal';
-                    element = '<input type="password" placeholder="password placeholder" name={{testInterpolated}} id="test-id" />';
-                    compileDirective();
+                    this.scope.testInterpolated = 'testVal';
+                    this.element = '<input type="password" placeholder="password placeholder" name={{testInterpolated}} id="test-id" />';
+                    this.compileDirective();
                 });
 
                 it('should create a substitute input of type text and use it as the placeholder', function () {
-                    expect(element.next().val()).toBe('password placeholder');
+                    expect(this.element.next().val()).toBe('password placeholder');
                 });
                 it('should copy the element\'s attributes to the substitute element and update them on change', function () {
-                    expect(element.attr('id')).toBe('test-id');
-                    expect(element.attr('name')).toBe('testVal');
+                    expect(this.element.attr('id')).toBe('test-id');
+                    expect(this.element.attr('name')).toBe('testVal');
 
-                    scope.testInterpolated = 'Changed Val';
-                    scope.$digest();
-                    expect(element.attr('name')).toBe('Changed Val');
+                    this.scope.testInterpolated = 'Changed Val';
+                    this.scope.$digest();
+                    expect(this.element.attr('name')).toBe('Changed Val');
                 });
                 it('should perform hide placeholder by hiding the element and showing the substitute', function () {
                     //set the display css property - it does not get a default in the test framework for some reason
-                    element.next().triggerHandler('focus');
-                    element.css('display', 'block');
-                    element.triggerHandler('blur');
+                    this.element.next().triggerHandler('focus');
+                    this.element.css('display', 'block');
+                    this.element.triggerHandler('blur');
 
-                    expect(element.css('display')).toBe('none');
-                    expect(element.next().css('display')).toBe('block');
+                    expect(this.element.css('display')).toBe('none');
+                    expect(this.element.next().css('display')).toBe('block');
 
-                    element.next().triggerHandler('focus');
-                    expect(element.css('display')).toBe('block');
-                    expect(element.next().css('display')).toBe('none');
+                    this.element.next().triggerHandler('focus');
+                    expect(this.element.css('display')).toBe('block');
+                    expect(this.element.next().css('display')).toBe('none');
                 });
                 it('should remove the substitute element on scope destroy', function () {
-                    expect(element.next().attr('type')).toBe('text');
-                    scope.$broadcast('$destroy');
-                    scope.$digest();
-                    expect(element.next().length).toBe(0);
+                    expect(this.element.next().attr('type')).toBe('text');
+                    this.scope.$broadcast('$destroy');
+                    this.scope.$digest();
+                    expect(this.element.next().length).toBe(0);
                 });
             });
         });
